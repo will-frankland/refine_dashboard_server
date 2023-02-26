13750d2c11455bbac4ef75c5638ca33aee431b37
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-26 17:20:29 -0500

    add userRouter and propertyRouter and initialize with app.use middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import userRouter from './routes/user.routes.js'
+import propertyRouter from './routes/property.routes.js';
 
 dotenv.config();
 
@@ -12,6 +14,9 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World!'})
 })
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/properties', propertyRouter);
 
 const startServer = async () => {
   try {
